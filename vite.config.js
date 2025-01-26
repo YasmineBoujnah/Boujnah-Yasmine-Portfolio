d41ab9c553fe5663c906da0189effd97058bc8e2
YasmineBoujnah <yasmineboujnah07@gmail.com>
2025-01-26 18:07:48 +0100

    still project academique

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/Boujnah-Yasmine-Portfolio/',
-=======
-  base: "/Boujnah-Yasmine-Portfolio",
->>>>>>> 232abe2913a7cc35df07f9823b986b7b043af1f6
+  base: '/Boujnah-Yasmine-Portfolio/', 
   plugins: [react()],
   css: {
     preprocessorOptions: {
